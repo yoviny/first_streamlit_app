650306879899d026e46087a259d2312d6740c0fa
Yovin Yahathugoda <yovin.yahathugoda@gmail.com>
2022-07-26 11:19:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
-import pandas
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 streamlit.title('My Parents New Healthy Diner')
 
